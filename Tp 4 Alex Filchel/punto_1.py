412e0731c9fa40cac6adf1f239515dc23b0ad6ca
AlexFilchel <alefilchel1010@gmail.com>
2024-10-04 11:05:15 -0300

    Agrego tp 4 Alex Filchel

---
+++ Tp 4 Alex Filchel/punto_1.py
@@ -0,0 +1,7 @@
+punto = (10,20)
+
+#a) Desempaqueta los valores de la tupla en las variables x y y.
+x , y = punto
+
+#b) Imprime el valor de cada variable.
+print(f"Las coordenadas son: {x} , {y}")
